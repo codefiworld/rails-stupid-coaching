efa51ce5ae01be2679f4c57ff58af5f56a8f06fb
Harsh Gambani <harsh.gambani@insead.edu>
2022-10-19 19:37:20 +0100

    completed

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,8 +3,18 @@ class QuestionsController < ApplicationController
   end
 
   def answer
-    inputData = params[:input];
-    
+    @question = params[:question]
+    @answer = coach_answer(@question);
+  end
+
+  def coach_answer(your_message)
+    if your_message.downcase == "i am going to work right now!"
+      "Great!"
+    elsif your_message.end_with?("?")
+      "Silly question, get dressed and go to work!"
+    else
+      "I don't care, get dressed and go to work!"
+    end
   end
 
 end
